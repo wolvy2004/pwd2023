4c0e047c577efc21eadacdf230d235dffecd691a
Eduardo Guerra <80855270+wolvy2004@users.noreply.github.com>
2023-05-23 20:46:52 -0300

    actualizacion de ModelBase

--- backend/src/Models/ModelBase.php
+++ backend/src/Models/ModelBase.php
@@ -6,7 +6,27 @@ use Raiz\Aux\Serializador;
 use Error;
 
 abstract class ModelBase implements Serializador
-{
+
+{   
+    private $id;
+    public function setId($id): mixed
+    {
+        if ($id === null) :
+            return 0;
+        else :
+            return intVal($id);
+        endif;
+    }
+
+    public function __construct(mixed $id)
+    {
+        $this->id=$this->setId($id);
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
     /** @return mixed[] */
     public function serializar(): array
     {
